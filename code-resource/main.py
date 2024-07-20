27a27cfee3aaa602c156c0075c128c84d1e237ac
ackwolver335 <abhaych335@gmail.com>
2024-07-20 13:39:21 +0530

    Features updated in Application

--- code-resource/main.py
+++ code-resource/main.py
@@ -2,6 +2,9 @@
 import tkinter as tk
 import tkinter.filedialog as fd
 import img2pdf as img
+import tkinter.messagebox as mg
+import os
+import shutil
 
 """
     This is an GUI Application designed in order to convert the images or General Image format to the pdf files.
@@ -11,6 +14,9 @@ import img2pdf as img
     1. Tkinter : It is general module used for designing the GUI Application in order to form a proper GUI Window.
     2. Tkinter.filedialog : This particular module from Tkinter Library is been used in order to choose a particular file from the Explorer.
     3. img2pdf : It is the main module which is been used for convertion of normal Image into a proper pdf file.
+    4. tkinter.messagebox : It is used for providing messagebox alert or warning regarding user unusual behaviour.
+    5. os : It is used for getting current location of the folder.
+    6. shutil : It is used for getting file moved from one location to another.
 
     Further Details regarding the Methods used in these Modules :
 
@@ -49,6 +55,9 @@ w1.geometry('300x190')                                  # Window's Dimension
 w1.resizable(False,False)                               # Unabling the Window to get Resizable
 w1.config(background = "#292826")                       # setting up background color
 
+# A general information and warning to the user
+mg.showinfo('Application Information','You must need to give the name of file and location before converting and after conversion only you have the choice to move the file Otherwise you may get an error !')
+
 # adding label to it
 lb1 = tk.Label(w1,text = 'Img2pdf Converter',font = ('Fira Code',10,'bold'),fg = "#F9D342",bg = "#292826")
 lb1.pack(padx = 2,pady = 10)
@@ -59,6 +68,20 @@ lb1.pack(padx = 2,pady = 10)
 def select():
     global files1                                                               # Specifying File Names variable to be Global
     files1 = fd.askopenfilenames(initialdir = '/',title = 'Select files')
+
+# defining a function or method for selecting file's location
+def select_location():
+    global file_location
+    file_location = fd.askdirectory()
+
+# defining move method for moving the file location to the directory that user wants
+def move_file():
+    if e1.get() == '' or file_location == '':
+        mg.showwarning('Application Error','Either the file is not renamed or Location is not selected !')
+    else:
+        new_location = file_location + "\\" + e1.get() + ".pdf"
+        current_dir = os.getcwd()  + "\\" + e1.get() + ".pdf"
+        shutil.move(current_dir,new_location)
 
 # adding some Button for the main purpose
 btn1 = tk.Button(w1,text = 'Select Image/Images',command = select,bg = "#F9D342",fg = "#292826")
@@ -86,12 +109,10 @@ def convert_single():
         
         # Specifying cases for file name if user try to attach extension
         if '.' in e1.get():
-            with open(f"file {index}.pdf","wb") as f:                                   # Opening file in binary write format
-                f.write(img.convert(file_name))                                         # Rewrite and converting into file
+            mg.showwarning('Application Warning','Do not use . while giving file name !')                                           # message warning to the user
 
-        elif e1.get() == "":
-            with open("single.pdf","wb") as f:                                          # Opening file in binary write format
-                f.write(img.convert(file_name))                                         # Rewrite and converting into file
+        elif e1.get() == "" or file_location == '':
+            mg.showwarning('Application Warning','Please fill out the name of pdf file or select file location !')                  # message warning to the user
         
         else:
             with open("{filename}.pdf".format(filename = e1.get()),"wb") as f:          # Opening file in binary write format
@@ -101,17 +122,19 @@ def convert_single():
 # In this multiple file will be converted into single pdf file
 
 def convert_multiple():
-    if e1.get() == '':
-        with open("file.pdf","wb") as f:                    # Opening Single Files in Write binary format
-            f.write(img.convert(files1))                    # Converting intot single file
+    if e1.get() == '' or file_location == '':
+        mg.showwarning('Application Warning','Please fill out the name of pdf file or select file location !')
 
     elif '.' in e1.get():
-        with open("file.pdf","wb") as f:                    # Opening Single Files in Write binary format
-            f.write(img.convert(files1))                    # Converting intot single file
+        mg.showwarning('Application Warning','Do not use . while giving file name !')
 
     else:
         with open("{file}.pdf".format(file = e1.get()),"wb") as f:                      # Opening Single Files in Write binary format
             f.write(img.convert(files1))                                                # Converting intot single file
+
+# button for selecting location as per user preferrence
+btn4 = tk.Button(frm1,text = 'Select Location',bg = "#F9D342",fg = "#292826",command = select_location)
+btn4.pack(padx = 3,pady = 3)
 
 # initiating Buttons for convertion of image or images
 btn2 = tk.Button(frm1,text = 'Convert Image',command = convert_single,bg = "#F9D342",fg = "#292826")              # Button for Single Image Conversion
@@ -119,5 +142,9 @@ btn2.pack(side = 'left',padx = 5)
 
 btn3 = tk.Button(frm1,text = 'Convert Images',command = convert_multiple,bg = "#F9D342",fg = "#292826")           # Button for Multiple Images Conversion
 btn3.pack(side = 'right',padx = 3)
+
+# adding button to move files using methods
+btn5 = tk.Button(w1,text = 'Move File',command = move_file,bg = "#F9D342",fg = "#292826")
+btn5.pack(padx = 3,pady = 3)
 
 w1.mainloop()
